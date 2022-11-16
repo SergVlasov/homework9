060105218634727c6841ead5ad854ca6a0f8f1c4
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-16 11:22:54 +0300

    Editing files

---
+++ task002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int M = InputText("Enter a number M: ");
+int N = InputText("Enter a number N: ");
+while (M > N)
+{
+    Console.WriteLine("The number M must be less than the number N");
+    M = InputText("Enter a number M: ");
+    N = InputText("Enter a number N: ");
+}
+Console.WriteLine($"Sum of numbers from {M} before {N} = {SumElements(M, N)}");
+
+int SumElements(int M, int N)
+{
+    int sum = 0;
+    if (M == N) return N;
+    else return sum += M + SumElements(M + 1, N);
+}
+
+int InputText(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
