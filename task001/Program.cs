092846f7974aa8866eedfe3f76bbf90e55bce954
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-16 11:13:28 +0300

    Editing files

---
+++ task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int N = InputText("Enter a number greater than 1: ");
+while (N < 1) N = InputText("Enter a number greater than 1: ");
+
+Console.WriteLine(NumbersFor(N));
+
+int InputText(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string NumbersFor(int N)
+{
+    if (N == 1) return "1";
+    else return N + "," + NumbersFor(N - 1);
+}
